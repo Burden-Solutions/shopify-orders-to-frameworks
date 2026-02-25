879a5036b87a345aa533d3bf579773d165be74d6
noucodes <escudero.elton@gmail.com>
2026-02-25 09:48:36 +0800

    feat: Added Starting Notification to logger

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const startServer = require('./server');
 const processOrder = require('./services/transform.service');
-const cron = require('node-cron');
-const config = require('./config/env');
+const webhookService = require('./logger/webhooks');
 
 startServer();
 console.log('Running order processing job...');
+webhookService.sendMessage('Starting order processing job...');
 processOrder();
 
